8d2ed5a4f4db2f27a7bf8c201738ed95ea9e1864
Dorothy <103705818+ValerieCat@users.noreply.github.com>
2025-09-03 20:55:09 -0500

    Agregé las 2 tareas del tema "Fundamentos de programacion" y re ordene nuevamente las carpetas

---
+++ Unidad1/3-09-25/Fundamentos de Programacion/CelciusToFarenheit.py
@@ -0,0 +1,7 @@
+# DANIEL ALEJANDRO HERNANDEZ MUÑOZ #24100507
+# Estructuras de datos 03/09/25
+print("Convertir celcius a Farenheit")
+celsius = float(input("Ingresa la temperatura en °C: "))
+fahrenheit = (celsius * 9/5) + 32
+print(celsius, "°C son", fahrenheit, "°F")
+input("Fin del Programa")
---
+++ Unidad1/3-09-25/Fundamentos de Programacion/CirculoRadio.py
@@ -0,0 +1,9 @@
+# DANIEL ALEJANDRO HERNANDEZ MUÑOZ #24100507
+# Estructuras de datos 03/09/25
+
+import math  
+
+radio = float(input("Ingresa el radio del círculo: "))
+area = math.pi * radio**2
+print("El área del círculo es:", area)
+input("Fin del Programa")
